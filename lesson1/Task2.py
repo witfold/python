2f8d65840163aeb921d2af0f614a8879c85ed6cf
Andrey Avtomenko <a.avtomenko@fundcount.com>
2020-10-01 10:25:43 +0700

    update task2

--- lesson1/Task2.py
+++ lesson1/Task2.py
@@ -2,15 +2,6 @@ seconds = int(input('Enter the number of seconds: '))
 
 hours = seconds // 3600
 minutes = (seconds - hours * 3600) // 60
-seconds -= hours * 3600 + minutes * 60
+seconds %= 60
 
-if hours < 10:
-    hours = '0' + str(hours)
-
-if minutes < 10:
-    minutes = '0' + str(minutes)
-
-if seconds < 10:
-    seconds = '0' + str(seconds)
-
-print(f'{hours}:{minutes}:{seconds}')
+print(f'{hours:02}:{minutes:02}:{seconds:02}')
