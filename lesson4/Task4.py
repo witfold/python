0c0ddf30eb8b7360e03c7b271df67ce3a72e0ad9
Andrey Avtomenko <a.avtomenko@fundcount.com>
2020-10-11 13:31:35 +0700

    update lesson4

--- lesson4/Task4.py
+++ lesson4/Task4.py
@@ -1,6 +1,6 @@
-import random
+from random import randint
 
-numbers = [random.randint(1, 10) for i in range(10)]
+numbers = [randint(1, 10) for _ in range(10)]
 
 print(f'Default list: {numbers}')
 print(f'Result: {[el for el in numbers if numbers.count(el) == 1]}')
